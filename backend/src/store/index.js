f0a2647ed586f8b4baf3128a157f75e7d8ea29a3
Giordani da Silveira dos Santos <dani.santossilveira@hotmail.com>
2023-06-27 19:27:14 -0300

    adicionado vue-router e vuex

---
+++ backend/src/store/index.js
@@ -0,0 +1,12 @@
+import {createStore} from 'vuex';
+
+const store = createStore({
+    state: {
+        teste: '1234567'
+    },
+    getters: {},
+    actions: {},
+    mutations: {}
+})
+
+export default store
